7f5b70b484940c13c55d9d28fbcb27405f16251c
Stephen Mckellar <stephen@thingmaker.io>
2016-08-06 16:57:33 +0100

    Basica hacker methods

--- hacker.py
+++ hacker.py
@@ -4,8 +4,14 @@ class Hacker
     @classmethod
     def discover():
 
+    def __init__(name, description):
+        self.name = name
+        self.description = description
+
     # hacker's name
     def name():
+        return self.name
 
     # hacker description
     def description():
+        return self.description
