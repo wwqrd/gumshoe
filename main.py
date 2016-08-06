dc400e1971bdfc5c79425cf58d8c193dfb7776ff
Joshua Melville <joshua.melville@oii.ox.ac.uk>
2016-08-06 22:48:03 +0100

    exp

--- main.py
+++ main.py
@@ -7,7 +7,7 @@
 
 
 # TODO
- # 
+ #
  # - opening screen: 'What is your hacker alias?' -- Can we get this from the device?
  # - Default screen to show user and their exp
  # - Shake device to search
@@ -27,23 +27,23 @@ from imu import IMU
 from database import database_set, database_get
 
 STRENGTH = ['gloriously bald', 'bearded', 'cute', 'feeble', 'wimpy', 'coy', 'lonely', 'emo', 'uber', 'mutant', 'evil', 'l33t'] # ajectives
-BREEDS = ['n00b', 'pokemon player', 'dweeb', 'nerd', 'geek', 'scriptkiddie', 'h4x0r', 'blackhat', 'whitehat', 'sysadmin', 'rogue A.I'] # nouns
+BREEDS = ['n00b', 'pokemon player', 'dweeb', 'nerd', 'geek', 'scriptkiddie', 'h4x0r', 'blackhat', 'sysadmin', 'rogue A.I'] # nouns
 
 def roll_dice(sides):
     return round((pyb.rng()/1073741824) * sides)
 
 class Hacker:
-    def __init__(self, name, strength):
+    def __init__(self, name, exp):
         self.name = name
-        self.strength = strength
+        self.exp = exp
 
     # hacker's name
     def get_name(self):
         return BREEDS[self.name]
 
-    # hacker strength
-    def get_strength(self):
-        return STRENGTH[self.strength]
+    # hacker exp
+    def get_exp(self):
+        return STRENGTH[self.exp]
 
 class Gumshoe:
     def __init__(self):
@@ -77,7 +77,7 @@ class Gumshoe:
             # You found a hacker!
             self.heat_factor = -3
             self.tail = Hacker(roll_dice(len(BREEDS) - 1), roll_dice(len(STRENGTH) - 1))
-            return "You found a " + self.tail.get_strength() + " " + self.tail.get_name() + "!"
+            return "You found a " + self.tail.get_exp() + " " + self.tail.get_name() + "!"
 
         # You find nothing
         self.heat_factor = self.heat_factor + 1
@@ -102,14 +102,24 @@ class Game:
     def inactive():
         return True
 
-    def search():
+    def search(self):
         ugfx.set_default_font(ugfx.FONT_MEDIUM_BOLD)
         ugfx.Label(5, 5, ugfx.width(), 20, "Scanning for hackers!...")
         ugfx.set_default_font(ugfx.FONT_NAME)
         ugfx.Label(5, 30, ugfx.width(), ugfx.height()-30, self.gumshoe.conduct_search())
 
-    def battle():
+    def battle(self, hacker):
+        # 320 x 240
+        #
+        # ugfx.set_default_font(ugfx.FONT_MEDIUM_BOLD)
+        # ugfx.Label(5, 5, ugfx.width(), 20, "Your oponent attacks!")
+        # ugfx.set_default_font(ugfx.FONT_NAME)
+        # ugfx.Label(5, 30, ugfx.width(), ugfx.height()-30, self.gumshoe.conduct_search())
+
         return True
+
+    def welcome(self):
+
 
 ugfx.init()
 buttons.init()
