69c9784ac938038267fa8c8c710e36fc0107d82d
Stephen Mckellar <stephen@thingmaker.io>
2016-08-06 21:38:02 +0100

    minified

--- external.py
+++
@@ -1,9 +0,0 @@
-needs_wifi = True
-update_rate = 120 * 1000
-
-utils = __import__("apps/gumshoe/utils")
-
-def tick():
-    print("whereami tick")
-    utils.store_location()
-    print("whereami tick done")
