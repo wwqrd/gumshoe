5d155591d7951f70890c687f8bf1670d2bb02fc6
Stephen Mckellar <stephen@thingmaker.io>
2016-08-06 16:51:33 +0100

    Check your six

--- gumshoe.py
+++ gumshoe.py
@@ -9,12 +9,13 @@ class Gumshoe:
         dice = randint(0,20) * hotness()
         if dice < 15:
             # You find nothing
+            return "You find nothing"
 
-        if dice > 15 and < 18:
+        if dice > 15 and dice < 18:
             # You are close!
             hotness_factor = 2
 
-        if dice > 20
+        if dice > 20:
             hotness_factor = 1
             # You found a hacker!
 
@@ -24,7 +25,8 @@ class Gumshoe:
 
     # which hackers has sleuth caught
     def captures():
+        return 0
 
     # multiplier for how likely sleuth will find something
     def hotness():
-        return hothess_factor
+        return hotness_factor
