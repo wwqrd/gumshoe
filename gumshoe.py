8e06134d12de43d75a2471800cbfbe22a21b49a6
Stephen Mckellar <stephen@thingmaker.io>
2016-08-06 15:56:29 +0100

    outline interfaces

---
+++ gumshoe.py
@@ -0,0 +1,13 @@
+class Gumshoe:
+
+    # find a hacker
+    def conduct_search():
+
+    # return sleuth skill
+    def skill():
+
+    # which hackers has sleuth caught
+    def captures():
+
+    # multiplier for how likely sleuth will find something
+    def hotness():
---
+++ hacker.py
@@ -0,0 +1,8 @@
+class Hacker
+
+    @classmethod
+    def discover():
+
+    def name():
+
+    def description():
