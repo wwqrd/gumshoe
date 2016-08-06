e9bf11c325b880a1fa11fa320ca6dee18db02cd1
Stephen Mckellar <stephen@thingmaker.io>
2016-08-06 16:46:29 +0100

    Gumshoe some more gumshoe

--- gumshoe.py
+++ gumshoe.py
@@ -1,13 +1,30 @@
+from random import randint
+
 class Gumshoe:
+
+    hotness_factor = 1
 
     # find a hacker
     def conduct_search():
+        dice = randint(0,20) * hotness()
+        if dice < 15:
+            # You find nothing
+
+        if dice > 15 and < 18:
+            # You are close!
+            hotness_factor = 2
+
+        if dice > 20
+            hotness_factor = 1
+            # You found a hacker!
 
     # return sleuth skill
     def skill():
+        return captures() * 7
 
     # which hackers has sleuth caught
     def captures():
 
     # multiplier for how likely sleuth will find something
     def hotness():
+        return hothess_factor
